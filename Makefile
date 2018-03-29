98ab787178b20d11c6494cec38d7011b5cb0e344
Lucas GUILLEROT <lguiller@e2r5p14.42.fr>
2018-03-29 16:08:28 +0200

    ++

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: lguiller <lguiller@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/01/16 12:18:12 by lguiller          #+#    #+#              #
-#    Updated: 2018/03/29 15:09:34 by lguiller         ###   ########.fr        #
+#    Updated: 2018/03/29 16:04:01 by lguiller         ###   ########.fr        #
 #    Updated: 2018/03/29 15:03:49 by lguiller         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
@@ -16,7 +16,7 @@
 ##################
 
 NAME		= wolf3d
-SRCS1		= main.c stock.c free_funct.c
+SRCS1		= main.c stock.c free_funct.c check_map.c
 SRCS		= $(addprefix $(SRCS_DIR), $(SRCS1))
 OBJS		= $(addprefix $(OBJS_DIR), $(SRCS1:.c=.o))
 SRCS_DIR	= srcs/
--- srcs/stock.c
+++ srcs/stock.c
@@ -6,11 +6,8 @@
 /*   By: lguiller <lguiller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 17:34:45 by lguiller          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/03/29 11:16:37 by lguiller         ###   ########.fr       */
-=======
+/*   Updated: 2018/03/29 15:12:28 by lguiller         ###   ########.fr       */
 /*   Updated: 2018/03/29 14:55:38 by lguiller         ###   ########.fr       */
->>>>>>> test
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +15,6 @@
 
 void	ft_read_file(char *name, char ***map)
 {
-<<<<<<< HEAD
-	char	fd;
-	char	*line;
-	int		i;
-
-	if ((fd = open(name, O_RDONLY)) == -1)
-		ft_error("error", 1, perror);
-	i = 0;
-	while (get_next_line(fd, &line))
-	{
-		ft_memcpy(map[0][i++], line, BUFF_SIZE);
-		ft_memdel((void **)&line);
-	}
-	ft_memdel((void **)&line);
-	close(fd);
-=======
 	t_parse		parse;
 	static char	start;
 
@@ -55,5 +36,4 @@ void	ft_read_file(char *name, char ***map)
 		ft_error("error: No start found", 1, ft_putendl_error);
 	if ((close(parse.fd)) == -1)
 		ft_error("error", 1, perror);
->>>>>>> test
 }
