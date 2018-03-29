4038cb06fbe81111533f566ec577dd12b68745eb
Lucas GUILLEROT <lguiller@e2r5p14.42.fr>
2018-03-29 15:04:51 +0200

    suppression des leaks

--- includes/wolf3d.h
+++ includes/wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: lguiller <lguiller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 17:05:59 by lguiller          #+#    #+#             */
-/*   Updated: 2018/03/29 13:35:06 by bede-fre         ###   ########.fr       */
+/*   Updated: 2018/03/29 15:03:40 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@ typedef struct	s_parse
 	char		fd;
 	char		*buff;
 	short		i;
-	int			line;
 }				t_parse;
 
-void		ft_check_map(char *buff, char *start);
-void		ft_read_file(char *name, char ***map);
-void		ft_free_map(char **map);
+void			ft_check_map(char *buff, char *start);
+void			ft_read_file(char *name, char ***map);
+void			ft_free_map(char **map);
 
 #endif
--- srcs/stock.c
+++ srcs/stock.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <lguiller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 17:34:45 by lguiller          #+#    #+#             */
-/*   Updated: 2018/03/29 13:36:07 by bede-fre         ###   ########.fr       */
+/*   Updated: 2018/03/29 14:55:38 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 void		ft_read_file(char *name, char ***map)
 {
-	static char	start;
 	t_parse		parse;
-	
+	static char	start;
+
 	if ((parse.fd = open(name, O_RDONLY)) == -1)
 		ft_error("error", 1, perror);
 	parse.i = 0;
-	parse.line = 0;
-	while (get_next_line(parse.fd, &parse.buff))
+	while (get_next_line(parse.fd, &parse.buff) > 0)
 	{
-		if (++parse.line > BUFF_SIZE)
-			ft_error("error: Wrong column length", 1, ft_putendl_error);
+		if (++parse.i > BUFF_SIZE)
+			ft_error("error: Wrong column length", 2, ft_putendl_error);
 		ft_check_map(parse.buff, &start);
-		ft_memcpy(map[0][parse.i++], parse.buff, BUFF_SIZE);
+		ft_strcpy(map[0][parse.i - 1], parse.buff);
 		ft_memdel((void **)&parse.buff);
 	}
-	if (parse.line < BUFF_SIZE)
+	ft_memdel((void **)&parse.buff);
+	if (parse.i < BUFF_SIZE)
 		ft_error("error: Wrong column lenght", 1, ft_putendl_error);
 	if (start == 0)
 		ft_error("error: No start found", 1, ft_putendl_error);
-	ft_memdel((void **)&parse.buff);
-	close(parse.fd);
+	if ((close(parse.fd)) == -1)
+		ft_error("error", 1, perror);
 }
