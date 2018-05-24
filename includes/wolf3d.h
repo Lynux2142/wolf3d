6173c747c1635ffec88069cfc3e17b4654cf9796
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-05-24 16:18:14 +0200

    reduction de le hitbox a 5 au lieu de 10

--- includes/wolf3d.h
+++ includes/wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: lguiller <lguiller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 17:05:59 by lguiller          #+#    #+#             */
-/*   Updated: 2018/05/23 09:41:38 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/05/24 16:15:55 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@
 # define WEST			M_PI
 # define EAST			0.0
 # define EAST2			2.0 * M_PI
-# define HIT_BOX		10.0
+# define HIT_BOX		5.0
 # define LITTLE			0.00000000000012
 # define TRUE			1
 # define FALSE			0
