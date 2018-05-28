775da31217bb045bbb8a3f65b10d1547fb9a81d0
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-05-28 11:17:00 +0200

    correction du bug de degrade sur la representation du champ de vision et ajout d'un conne de vision

--- srcs/bresenham.c
+++ srcs/bresenham.c
@@ -6,13 +6,13 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 13:28:36 by bede-fre          #+#    #+#             */
-/*   Updated: 2018/04/27 17:48:56 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/05/28 11:15:13 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-static void	ft_fill_line(t_algo_brez algo, t_img *ptr, int col)
+static void	ft_fill_line(t_algo_brez algo, t_img *ptr, int col, t_player *p)
 {
 	algo.cumul = algo.da / 2;
 	while (algo.i <= algo.da)
@@ -24,14 +24,15 @@ static void	ft_fill_line(t_algo_brez algo, t_img *ptr, int col)
 			algo.cumul -= algo.da;
 			algo.b += algo.cptb;
 		}
-		if (col != 0)
-			col -= 0x050500;
+		if (20.0 < sqrtf(powf((double)algo.a - (p->x / 8.0), 2.0) +
+		powf((p->y / 8.0) - (double)algo.b, 2.0)))
+			break ;
 		ft_fill_pixel(ptr, algo.a, algo.b, col);
 		algo.i++;
 	}
 }
 
-static void	ft_fill_column(t_algo_brez algo, t_img *ptr, int col)
+static void	ft_fill_column(t_algo_brez algo, t_img *ptr, int col, t_player *p)
 {
 	algo.cumul = algo.db / 2;
 	while (algo.i <= algo.db)
@@ -43,8 +44,9 @@ static void	ft_fill_column(t_algo_brez algo, t_img *ptr, int col)
 			algo.cumul -= algo.db;
 			algo.a += algo.cpta;
 		}
-		if (col != 0)
-			col -= 0x050500;
+		if (20.0 < sqrtf(powf((double)algo.a - (p->x / 8.0), 2.0) +
+		powf((p->y / 8.0) - (double)algo.b, 2.0)))
+			break ;
 		ft_fill_pixel(ptr, algo.a, algo.b, col);
 		algo.i++;
 	}
@@ -65,7 +67,7 @@ void		ft_algo(t_img *ptr, t_ray ray, t_player *p, int col)
 	algo.i = 1;
 	ft_fill_pixel(ptr, algo.a, algo.b, col);
 	if (algo.da > algo.db)
-		ft_fill_line(algo, ptr, col);
+		ft_fill_line(algo, ptr, col, p);
 	else
-		ft_fill_column(algo, ptr, col);
+		ft_fill_column(algo, ptr, col, p);
 }
